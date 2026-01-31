e274a7a1a7eafab9a64ceb0d608ac23f7e5ea6ba
Abolfazl Jafari M <abolfazljafari563@gmail.com>
2026-01-31 11:33:13 +0330

    fix bug

--- src/components/userInfo.jsx
+++ src/components/userInfo.jsx
@@ -3,7 +3,7 @@ import {useTelegram} from "../hooks/useTelegram.js";
 function UserInfo() {
     const tg = useTelegram();
 
-    if (!tg && !tg.initDataUnsafe) return null;
+    if (!tg && !(tg?.initDataUnsafe)) return null;
     return (
         <div className={"flex flex-col gap-2"}>
             <div className={"flex items-center gap-1"}>
