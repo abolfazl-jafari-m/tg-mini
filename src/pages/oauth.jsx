f913a07f24da11128182591a9f28544793d6189b
Abolfazl Jafari M <abolfazljafari563@gmail.com>
2026-01-31 13:26:03 +0330

    oauth btn

--- src/pages/oauth.jsx
+++ src/pages/oauth.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import TelegramLogin from "../components/telegram-login.jsx";
+import {LoginButton} from "@telegram-auth/react";
 
 function Oauth() {
+    const handleAuth = (response)=>{
+        console.log(response);
+    }
     return (
         <div>
             <TelegramLogin/>
+            <LoginButton botUsername={"ajm_test_bot"} onAuthCallback={handleAuth}/>
         </div>
     );
 }
