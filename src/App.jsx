cf384ff52a24e41a829e4c1a85af38b806c8e20c
Abolfazl Jafari M <abolfazljafari563@gmail.com>
2026-01-31 11:45:53 +0330

    test for vibrate

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
                             onClick={handleContact}>request Contacts
                     </button>
                     <button className={"border-white rounded-md px-4 py-0.5 bg-yellow-800 text-white"}
-                            onClick={()=>tg.impactOccurred("heavy")}>shake it
+                            onClick={()=>tg.HapticFeedback.impactOccurred("heavy")}>shake it
                     </button>
                     <BiometricManger/>
                 </div>
