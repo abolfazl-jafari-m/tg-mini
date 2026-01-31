fcdec7bf4b5c499b78d701b5215ef341b647e1a0
Abolfazl Jafari M <abolfazljafari563@gmail.com>
2026-01-31 10:25:42 +0330

    test for request contact

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,21 @@ import {safe} from "./lib/helper.ts";
 function App() {
   const tg = useTelegram();
 
-  console.log(tg);
+  const handleContact =async ()=>{
+      if (!tg) return;
+      try {
+        const contact = await tg.requestContact();
+      }catch {
+          tg.showAlert("permission denied");
+      }
+  }
 
 
   return (
     <>
       <h1 className={"text-green-700"}>This is a Test for Tg mini</h1>
-      <button onClick={()=>safe(tg.showPopup({title : "test" , message : "this is a test for popup"}) , null)}>show popup</button>
+      <button className={"text-white"} onClick={()=>safe(tg.showPopup({title : "test" , message : "this is a test for popup"}) , null)}>show popup</button>
+        <button className={"border-white rounded-2xl px-4 py-0.5 bg-rose-800 text-white"} onClick={handleContact}>request Contacts</button>
     </>
   )
 }
